acaed3ed5388680023194b73daddd7978ff7fcac
eliak09 <avatar_eliak@hotmail.com>
2026-04-06 00:51:22 -0600

    lil changes uwu

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,10 +1,12 @@
+// Variables con prompt
 let num1 = prompt("Ingresa el primer número: ");
 let num2 = prompt("Ingresa el segundo número: ");
 let num3 = prompt("Ingresa el tercer número: ");
 
+// convertir los números a int. Array para ordenar.
 let numbers = [parseInt(num1), parseInt(num2), parseInt(num3)];
 
-//verifcar igualdades aquí
+// Verifica todas las posibilidades de igualdad con alerta y console.log
 if (numbers[0] === numbers[1] && numbers[0] === numbers[2]) {
     alert("Los tres números son iguales.");
     console.log("Los tres números son iguales.");
@@ -19,12 +21,15 @@ if (numbers[0] === numbers[1] && numbers[0] === numbers[2]) {
     console.log("El segundo y tercer número son iguales.");
 }
 
+// sort ordena de menor a mayor
 numbers.sort((a, b) => a - b);
 console.log("Menor a mayor: " + numbers);
 
+//reverse ordena de mayor a menor. Agreado el operador spread no interferir en el array original.
 let numbersDesc = [...numbers].reverse();
 console.log("Mayor a menor: " + numbersDesc);
 
+//alerta y console.log para cada número según la posición.
 alert("El número menor es: " + numbers[0]);
 alert("El número central es: " + numbers[1]);
 alert("El número mayor es: " + numbers[2]);
